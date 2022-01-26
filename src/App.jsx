e87d956b52531d14d8be45388a000d9d571e53ae
sherry <ssheheryar12@gmail.com>
2022-01-26 19:13:22 +0500

    adding code format

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,22 @@
 import React from 'react';
 import { useState } from 'react'; //import usestatae
 
-
-
-
 const App = () => {
-
   let newTime = new Date().toLocaleTimeString();
-const [cTime, setTime] = useState(newTime);
+  const [cTime, setTime] = useState(newTime);
 
-const updateTime = () => {
-  newTime = new Date().toLocaleTimeString();
-  setTime(newTime);
-};
+  const updateTime = () => {
+    newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  };
 
-  setInterval(updateTime, 1000 );         //update time is callback function which used second how many time function callback//
+  setInterval(updateTime, 1000); //update time is callback function which used second how many time function callback//
 
   return (
-  <>
-  <h1> {cTime}</h1>
- 
-</>
-);
+    <>
+      <h1> {cTime}</h1>
+    </>
+  );
 };
-
-
-
 
 export default App;
