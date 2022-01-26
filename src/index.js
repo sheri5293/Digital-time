445425dc317f0bea80db9f0d4c2e272cb0b09964
sherry <ssheheryar12@gmail.com>
2022-01-26 19:02:44 +0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+import App from "./App";
+
+
+
+
+
+
+
+
+//define a fnction//
+
+
+
+
+
+ReactDOM.render (
+
+<>
+
+<App/>
+
+</>,
+
+
+  document.getElementById('root'));
+
+
+
+
