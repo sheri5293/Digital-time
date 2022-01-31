138097c3103b71a7aab9fc59938610586e20b3d3
sherry <ssheheryar12@gmail.com>
2022-01-31 17:08:20 +0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,98 +1,44 @@
 import React from 'react';
-import { useState } from 'react'; //import usestatae
+import { useState } from 'react';
+import ToDolist from './ToDolist';
 
 const App = () => {
-  const [FullName, setFullName] = useState({      //use state pass in an object//
-    fname: '',
-    lName: '',
-    eMail: '',
-    phone: ''
-  }); //useState is a hook
+  const [inputList, setInputList] = useState('');
+  const [List, setList] = useState([]); // now it become Array
 
+  const itemEvent = (e) => {
+    setInputList(e.target.value);
+  };
 
-  const eventPass = (e) => {      // object pass in event
-        
-//object diestrcucting//
-    const {value,name} = e.target;
-
-
-    setFullName((preValue)=>{
-
-return{
-  ...preValue,
-  [name] : value,      // its mean jo prevalue ka andar object hain un ka name iss name sa mill raha ha tu us ki  value show kr do//
-};
-           
-          
+  const ListItem = () => {
+    setList((oldItems) => {
+      return [...oldItems, inputList];
     
+    });
+    setInputList('');
+  };
 
+  return (
+    <>
+      <div className='main_div'>
+        <div className='center_div'>
+          <br />
+          <h1>To do List</h1>
+          <br />
+          <input type='text' placeholder='Enter a items' onChange={itemEvent}  value={inputList}/>
+          <button onClick={ListItem}>+</button>
+          <ol>
+            {/* map function //Attitube (1st curent value... 2nd current value index no ... 3rd Array deal with us )  */}
 
-      // if(name==='fname'){
-      //   return{
-      //     fname: value,
-      //     lName: preValue.lName,
-      //      eMail: preValue.eMail,
-      //      phone: preValue.phone
-      //   };  
-      //   }else if (name==='lName'){
-      //     return{
-      //       fname: preValue.fname,
-      //       lName: value,
-      //        eMail: preValue.eMail,
-      //        phone: preValue.phone
-      //     };
-      //   }else if (name==='eMail'){
-      //     return{
-      //       fname: preValue.fname,
-      //       lName: preValue.lName,
-      //        eMail: value,
-      //        phone: preValue.phone
-      //     };
-      //   }else if (name==='phone'){
-      //     return{
-      //       fname: preValue.fname,
-      //       lName: preValue.fname,
-      //        eMail: preValue.eMail,
-      //         phone: value
-      //     };
-      //   }
-  });
-};
-
-const onSubmits=(e)=> {
-  e.preventDefault();
-  alert('form submitted');
-
-};
-
-
-
-
-
-  return(
-<>
-<div>
-  <h1>  {FullName.fname} {FullName.lName}  </h1>
-  <p> {FullName.eMail}</p>
-  <p> {FullName.phone}</p>
-
-  </div>
-  <form onSubmit={onSubmits}>                                                
-<input type="text"  placeholder='Enter your name' name='fname' onChange={eventPass} value={FullName.fname}/>                   
-{/* //!curent value is fullname so we use fullNammefname */}
-
-<br></br>
-<input type="text"  placeholder='Enter your last name' name='lName' onChange={eventPass} value={FullName.lName}/>
-<br></br>
-<input type="email"  placeholder='Enter your last Email' name='eMail' onChange={eventPass} value={FullName.eMail}/>
-<br></br>
-<input type="number"  placeholder='Enter your last phone' name='phone' onChange={eventPass} value={FullName.phone}/>
-<br></br>
-<button type='submit'> click me  </button>
-
-</form>
-</>
- );
+            {List.map((itemValue) => {          //and call the array
+              return  <ToDolist
+                text={itemValue} />;
+            })}
+          </ol>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
